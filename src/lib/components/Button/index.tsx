0ad80e05560e0d3dc552fd8bb7c0dc1bb77f30e9
Joo Han Kim <joo-han.kim@hermesworld.com>
2023-04-04 13:28:45 +0200

    Base setup files

---
+++ src/lib/components/Button/index.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+export interface Props {
+  something: boolean
+}
+
+function Button() {
+  return <div>Button</div>
+}
+
+export default Button
---
+++ src/lib/index.ts
@@ -0,0 +1,2 @@
+import "./theme.css"
+export * from "./components"
