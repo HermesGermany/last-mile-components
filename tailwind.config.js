5e25902af356c72bffdbf4b2b3afe2812b55b3a2
Joo Han Kim <joo-han.kim@hermesworld.com>
2023-04-04 13:09:29 +0200

    Added hermes colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "hermes-blue": "var(--hermes-blue)",
+        "hermes-lightblue": "var(--hermes-lightblue)",
+        "hermes-orange": "var(--hermes-orange)",
+        "hermes-orange-hover": "var(--hermes-orange-hover)",
+      },
+      fontFamily: {
+        marselis: ["Marselis"],
+      },
+    },
   },
   plugins: [],
-};
-
+}
