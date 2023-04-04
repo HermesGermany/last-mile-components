6f0cd9eee679616e7a610c77e266091000a2d26c
Joo Han Kim <joo-han.kim@hermesworld.com>
2023-04-04 13:09:04 +0200

    Added vite dts plugin and library build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,45 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react"
+import path from "node:path"
+import { defineConfig } from "vitest/config"
+import dts from "vite-plugin-dts"
+import tailwindcss from "tailwindcss"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
   resolve: {
     preserveSymlinks: true,
   },
-});
+  css: {
+    postcss: {
+      plugins: [tailwindcss],
+    },
+  },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/lib/index.ts"),
+      name: "lmc",
+      formats: ["es", "umd"],
+      fileName: (format) => `lmc.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom", "tailwindcss"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+          tailwindcss: "tailwindcss",
+        },
+      },
+    },
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+  },
+})
