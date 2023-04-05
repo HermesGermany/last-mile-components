b954be22a304de335741e0ff546b97bd3c2d0a1d
padidas <paddybaber@gmail.com>
2023-04-05 18:20:27 +0200

    add tailwind to storybook
    Co-authored-by: joobacca <joohankim@gmx.de>

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,19 +1,17 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    "@storybook/addon-postcss",
   ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-vite",
-    "disableTelemetry": true
+  framework: "@storybook/react",
+  core: {
+    builder: "@storybook/builder-vite",
+    disableTelemetry: true,
   },
-  "features": {
-    "storyStoreV7": true
-  }
+  features: {
+    storyStoreV7: true,
+  },
 }
--- .storybook/preview.cjs
+++ .storybook/preview.cjs
@@ -1,3 +1,5 @@
+import "../src/lib/theme.css"
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
