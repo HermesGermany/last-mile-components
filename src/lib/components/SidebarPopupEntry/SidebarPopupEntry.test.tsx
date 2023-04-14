d03ac910c26d3d90a5615769a6e298e63b301873
padidas <paddybaber@gmail.com>
2023-04-14 15:57:00 +0200

    formatting

--- src/lib/components/SidebarPopupEntry/SidebarPopupEntry.test.tsx
+++ src/lib/components/SidebarPopupEntry/SidebarPopupEntry.test.tsx
@@ -2,6 +2,8 @@ import { render } from "@testing-library/react"
 import SidebarPopupEntry from "./SidebarPopupEntry"
 
 test("renders", () => {
-  const { getByTestId } = render(<SidebarPopupEntry data-testid="sidebar-popup-entry" />)
+  const { getByTestId } = render(
+    <SidebarPopupEntry data-testid="sidebar-popup-entry" />
+  )
   expect(getByTestId("sidebar-popup-entry")).toBeDefined()
 })
