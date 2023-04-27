0e8b177f1df65213acb398a9c6fe998e9c195335
padidas <paddybaber@gmail.com>
2023-04-27 08:46:05 +0200

    sidebar story

--- src/lib/components/Sidebar/Sidebar.stories.tsx
+++ src/lib/components/Sidebar/Sidebar.stories.tsx
@@ -79,16 +79,34 @@ FullExample.args = {
       <AppsMenu popoverTitle="Andere Apps" apps={appsList} />
       <BurgerMenu>
         <Popover.MenuGroup groupLabel="Group 1">
-          <Popover.MenuItem label="Test" action={() => alert("Clicked")} />
-          <Popover.MenuItem label="Test" action={() => alert("Clicked")} />
-          <Popover.MenuItem label="Test" action={() => alert("Clicked")} />
-          <Popover.MenuItem label="Test" action={() => alert("Clicked")} />
+          <Popover.MenuItem
+            label="Menu item short 1"
+            action={() => alert("Clicked")}
+          />
+          <Popover.MenuItem
+            label="Menu item short 2"
+            action={() => alert("Clicked")}
+          />
+          <Popover.MenuItem
+            label="Menu item with long text"
+            action={() => alert("Clicked")}
+          />
+          <Popover.MenuItem
+            label="Menu item with very very very long text"
+            action={() => alert("Clicked")}
+          />
         </Popover.MenuGroup>
         <Popover.MenuGroup groupLabel="Group 2">
-          <Popover.MenuItem label="Test" action={() => alert("Clicked")} />
+          <Popover.MenuItem
+            label="Menu item short 3"
+            action={() => alert("Clicked")}
+          />
         </Popover.MenuGroup>
         <Popover.MenuGroup groupLabel="Group 3">
-          <Popover.MenuItem label="Test" action={() => alert("Clicked")} />
+          <Popover.MenuItem
+            label="Menu item short 4"
+            action={() => alert("Clicked")}
+          />
         </Popover.MenuGroup>
       </BurgerMenu>
     </>
