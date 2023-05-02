fbc8c4190f9ab97ee9a7b7fe7707e901bef9c701
padidas <paddybaber@gmail.com>
2023-05-02 14:47:37 +0200

    sidebarbtn: justify between if btn has label&&icon

--- src/components/SidebarButton.tsx
+++ src/components/SidebarButton.tsx
@@ -19,8 +19,10 @@ const SidebarButton = ({
 }: SidebarButtonProps) => {
   return (
     <ButtonComponent
-      className={`tw-box-border tw-flex tw-w-full tw-cursor-pointer tw-flex-col tw-items-center tw-justify-center tw-gap-1 tw-rounded tw-border-none tw-bg-transparent tw-px-0 tw-py-1 tw-text-[10px] tw-font-bold tw-text-white tw-outline-none hover:tw-bg-white hover:tw-bg-opacity-10 ${
+      className={`tw-box-border tw-flex tw-w-full tw-cursor-pointer tw-flex-col tw-items-center tw-rounded tw-border-none tw-bg-transparent tw-px-0 tw-py-1 tw-text-[10px] tw-font-bold tw-text-white tw-outline-none hover:tw-bg-white hover:tw-bg-opacity-10 ${
         icon ? "tw-aspect-square" : "tw-py-3"
+      } ${
+        icon && label ? "tw-justify-between" : "tw-justify-center"
       } ${className}`}
       onClick={onClick}
     >
