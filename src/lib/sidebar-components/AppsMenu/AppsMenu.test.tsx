098a811abddb88340b5e22da737e36fc6e738894
joobacca <joohankim@gmx.de>
2023-05-15 16:45:45 +0200

    Added unit tests for AppsMenu

--- src/lib/sidebar-components/AppsMenu/AppsMenu.test.tsx
+++ src/lib/sidebar-components/AppsMenu/AppsMenu.test.tsx
@@ -1,7 +1,44 @@
-import { render } from "@testing-library/react"
-import AppsMenu from "./AppsMenu"
+import { composeStories } from "@storybook/react"
+import { act, render, screen } from "@testing-library/react"
+import { appsList } from "../../../storybook-helper-components/appsList"
+import * as stories from "./AppsMenu.stories"
+
+const { Basic } = composeStories(stories)
+const testId = "apps-menu"
 
 test("renders", () => {
-  const { getByTestId } = render(<AppsMenu data-testid="apps-menu" apps={[]} />)
-  expect(getByTestId("apps-menu")).toBeDefined()
+  const { getByTestId } = render(<Basic data-testid={testId} apps={[]} />)
+  expect(getByTestId(testId)).toBeDefined()
+})
+
+test("renders with data", async () => {
+  const { getByText } = render(<Basic />)
+  act(() => {
+    getByText("Apps").click()
+  })
+
+  for (const { label } of appsList) {
+    expect(await screen.findByText(label)).toBeDefined()
+  }
+})
+
+test("shows custom popover title", async () => {
+  const customTitle = "The tales of foo and bar"
+  const { getByText } = render(<Basic popoverTitle={customTitle} />)
+
+  act(() => {
+    getByText("Apps").click()
+  })
+  expect(await screen.findByText(customTitle)).toBeDefined()
+})
+
+test("passes the button props along to the button", async () => {
+  const { getByTestId } = render(
+    <Basic
+      buttonProps={{
+        "data-testid": "apps-button",
+      }}
+    />
+  )
+  expect(getByTestId("apps-button")).toBeDefined()
 })
