f366cb8276ff946f577a823c50236cd5e8c3b201
joobacca <joohankim@gmx.de>
2023-05-16 08:59:28 +0200

    BurgerMenu unit test added

--- src/lib/sidebar-components/BurgerMenu/BurgerMenu.test.tsx
+++ src/lib/sidebar-components/BurgerMenu/BurgerMenu.test.tsx
@@ -5,3 +5,13 @@ test("renders", () => {
   const { getByTestId } = render(<BurgerMenu data-testid="burger-menu" />)
   expect(getByTestId("burger-menu")).toBeDefined()
 })
+
+test("shows the correct icon", () => {
+  const { container } = render(<BurgerMenu data-testid="burger-menu" />)
+  expect(container.querySelector("svg")).toBeDefined()
+})
+
+/**
+ * No further tests necessary, since BurgerMenu is just one
+ * implementation of Popover with a specific button icon
+ */
