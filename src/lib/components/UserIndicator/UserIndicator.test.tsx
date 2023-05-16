228da0c90d74cea8f840def65a1fde26e65e6e3f
padidas <paddybaber@gmail.com>
2023-05-16 10:58:38 +0200

    remove describte from tests with just one usecase

--- src/lib/components/UserIndicator/UserIndicator.test.tsx
+++ src/lib/components/UserIndicator/UserIndicator.test.tsx
@@ -1,37 +1,33 @@
 import { render, screen } from "@testing-library/react"
 import UserIndicator from "./UserIndicator"
 
-test("renders", () => {
+it("renders", () => {
   const { getByTestId } = render(<UserIndicator data-testid="user-indicator" />)
   expect(getByTestId("user-indicator")).toBeDefined()
 })
 
-describe("UserIndicator", () => {
-  it("renders the user's initials", () => {
-    renderUserIndicator()
-    const initials = screen.getByText("MM")
-    expect(initials).toBeDefined()
-  })
+it("renders the user's initials", () => {
+  renderUserIndicator()
+  const initials = screen.getByText("MM")
+  expect(initials).toBeDefined()
+})
 
-  it("renders full name and username in title-attribute", () => {
-    renderUserIndicator()
-    const title = screen.getByTitle(
-      "Eingeloggt als Max Mustermann (mustermaxx)"
-    )
-    expect(title).toBeDefined()
-  })
+it("renders full name and username in title-attribute", () => {
+  renderUserIndicator()
+  const title = screen.getByTitle("Eingeloggt als Max Mustermann (mustermaxx)")
+  expect(title).toBeDefined()
+})
 
-  it("renders no initials when first and last name are not provided", () => {
-    renderUserIndicator("", "")
-    const initialsElement = screen.queryByText(/.+/) // Regex to match any text
-    expect(initialsElement).toBeNull()
-  })
+it("renders no initials when first and last name are not provided", () => {
+  renderUserIndicator("", "")
+  const initialsElement = screen.queryByText(/.+/) // Regex to match any text
+  expect(initialsElement).toBeNull()
+})
 
-  it("renders no initials when first and last name are not provided", () => {
-    renderUserIndicator("", "")
-    const title = screen.getByTitle(/Eingeloggt als\s+\(mustermaxx\)/) // Regex to match one or more whitespace chars
-    expect(title).toBeDefined()
-  })
+it("renders no initials when first and last name are not provided", () => {
+  renderUserIndicator("", "")
+  const title = screen.getByTitle(/Eingeloggt als\s+\(mustermaxx\)/) // Regex to match one or more whitespace chars
+  expect(title).toBeDefined()
 })
 
 function renderUserIndicator(
