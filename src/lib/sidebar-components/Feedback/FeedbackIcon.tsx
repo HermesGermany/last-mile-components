e8058fae244bcd264f3966b9ebd5cdefc69b1ebf
joobacca <joohankim@gmx.de>
2023-06-20 15:46:31 +0200

    Fixed lint errors

--- src/lib/sidebar-components/Feedback/FeedbackIcon.tsx
+++ src/lib/sidebar-components/Feedback/FeedbackIcon.tsx
@@ -1,6 +1,12 @@
 function FeedbackIcon() {
   return (
-    <svg width="20" height="22" viewBox="0 0 20 22" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+    <svg
+      width="20"
+      height="22"
+      viewBox="0 0 20 22"
+      fill="currentColor"
+      xmlns="http://www.w3.org/2000/svg"
+    >
       <path
         d="M18 2H2.01001C0.91001 2 0.0100098 2.9 0.0100098 4V22L4.00001 18H18C19.1 18 20 17.1 20 16V4C20 2.9 19.1 2 18 2ZM11 14H9.00001V12H11V14ZM11 9C11 9.55 10.55 10 10 10C9.45001 10 9.00001 9.55 9.00001 9V7C9.00001 6.45 9.45001 6 10 6C10.55 6 11 6.45 11 7V9Z"
         fill="currentColor"
--- src/lib/sidebar-components/Feedback/SuccessIcon.tsx
+++ src/lib/sidebar-components/Feedback/SuccessIcon.tsx
@@ -1,6 +1,12 @@
 function SuccessIcon() {
   return (
-    <svg width="16" height="12" viewBox="0 0 16 12" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+    <svg
+      width="16"
+      height="12"
+      viewBox="0 0 16 12"
+      fill="currentColor"
+      xmlns="http://www.w3.org/2000/svg"
+    >
       <path
         d="M3.05028 3.2929L0.928955 5.41423L6.58581 11.0711L15.0711 2.5858L12.9498 0.464478L6.58581 6.82844L3.05028 3.2929Z"
         fill="currentColor"
