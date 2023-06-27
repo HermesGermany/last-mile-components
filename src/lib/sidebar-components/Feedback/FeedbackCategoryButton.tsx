0dbf2e8bb4b1513e42ab8cabcb26aefb449426f5
padidas <paddybaber@gmail.com>
2023-06-27 08:49:11 +0200

    change font and font-width of feedback cat btns

--- src/lib/sidebar-components/Feedback/FeedbackCategoryButton.tsx
+++ src/lib/sidebar-components/Feedback/FeedbackCategoryButton.tsx
@@ -19,9 +19,9 @@ export default function FeedbackCategoryButton({
       data-testid={`feedback-category-${feedbackCategory}`}
       className={clsx(
         `tw-flex tw-h-11 tw-w-full tw-cursor-pointer tw-items-center tw-justify-center tw-rounded-sm tw-border-none`,
-        "tw-px-1.5 tw-py-2.5 tw-shadow-sm hover:tw-bg-hermes-blue-hover hover:tw-text-white disabled:tw-cursor-default disabled:tw-bg-hermes-blue-light",
+        "tw-px-1.5 tw-py-2.5 tw-font-sans tw-shadow-sm hover:tw-bg-hermes-blue-hover hover:tw-text-white disabled:tw-cursor-default disabled:tw-bg-hermes-blue-light",
         active
-          ? "tw-bg-hermes-blue tw-font-marselis tw-text-white"
+          ? "tw-bg-hermes-blue tw-font-medium tw-text-white"
           : "tw-bg-hermes-grey-5 tw-text-hermes-grey"
       )}
       onClick={() =>
